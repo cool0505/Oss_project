f6398f4c9c64c97c0fa400db3bd58c5ea285b3e9
KOH JOOWON <joowonkoh@naver.com>
2023-01-21 22:26:04 +0900

    update home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path='/' element={<Home/>} />
         <Route path='/Login' element={<Login/>} />
         <Route path='/Signup' element={<Signup/>} />
       </Routes>
